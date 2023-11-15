7fa5963ece3e02025740e1a627519a7bdad2af54
Nico <nico.s@kabsi.at>
2023-11-15 13:52:53 +0100

    Minor changes of index.html, PasswordController.java, styles.css Clean up code

--- src/main/java/com/strauss/passwordgenerator/PasswordController.java
+++ src/main/java/com/strauss/passwordgenerator/PasswordController.java
@@ -4,8 +4,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Scanner;
-
 @RestController
 public class PasswordController {
 
